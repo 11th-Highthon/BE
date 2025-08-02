6a2fd288ecdb2ba11d5a422e0859ff819b930324
suhun <yunsuhun518@naver.com>
2025-08-02 20:05:12 +0900

    regist,login

--- src/services/userServiece.ts
+++ src/services/userServiece.ts
@@ -1,7 +1,9 @@
+import { LoginDto } from "../dto/user/login-dto";
 import { registerUserDto } from "../dto/user/register-dto";
 import { IUser } from "../interfaces/IUser";
 import * as userRepository from "../repositories/userRepository";
 import * as bcrypt from "bcrypt";
+import * as jwt from "jsonwebtoken";
 
 export const findByEmail = async (email: string): Promise<IUser> => {
   const existingUser = await userRepository.findOneByEmail(email);
@@ -22,3 +24,28 @@ export const save = async (user: registerUserDto): Promise<IUser> => {
   user.password = bcrypt.hashSync(user.password, 10);
   return await userRepository.save(user.toEntity());
 };
+
+export const login = async (loginDto: LoginDto): Promise<string> => {
+    const user = await userRepository.findOneByEmail(loginDto.email);
+    if(!user) {
+        throw new Error("User not found"); 
+    }
+    const isPasswordValid = bcrypt.compareSync(loginDto.password, user.password);
+    if (!isPasswordValid) {
+        throw new Error("Invalid password");
+    }
+    const token = jwt.sign({username: user.username, email: user.email}, "secretKey",{
+        expiresIn: "1h"
+    });
+    return token;
+}
+
+export const verifyToken = async (token: string): Promise<IUser> => {
+    const decoded = jwt.verify(token, "secretKey") as { username: string; email: string };
+    const user = await findByEmail(decoded.email);
+    if (!user) {
+        throw new Error("User not found");
+    } 
+    return user;
+
+}
