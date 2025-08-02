d5d09df5c2d9cbd05e7dc5abc4e8a81649d76032
balhyo-younjisang <yjs12180825@gmail.com>
2025-08-02 17:09:07 +0900

    feat: init express

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+import express from 'express';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const app = express();
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log(`Server is running on port http://localhost:${process.env.PORT || 3000}`);
+});
