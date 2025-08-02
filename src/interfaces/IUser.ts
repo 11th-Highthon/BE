9b2714f2fd5eb198d0347212af87d7991d5e66fd
balhyo-younjisang <yjs12180825@gmail.com>
2025-08-02 17:54:45 +0900

    feat(interfaces): add User interface

---
+++ src/interfaces/IUser.ts
@@ -0,0 +1,9 @@
+import mongoose from "mongoose";
+
+export interface IUser extends mongoose.Document {
+    username: string;
+    password: string;
+    email: string;
+    createdStories: mongoose.Types.ObjectId[];
+    playedStories: mongoose.Types.ObjectId[];
+}
